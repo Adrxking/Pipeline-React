53551f7f50453bf66053f21d52afca8cad55ad67
adrxking <adrianalbujaira@gmail.com>
2022-02-14 18:42:20 +0100

    deleted build

--- services/scripts/deploy.sh
+++ services/scripts/deploy.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env sh
 
+#set -x
+#cd app/react
+#npm run build
+#set +x
+#
+#echo 'El comando "npm" ejecuta la app Node.js/React.'
+#echo 'El "npm start" tiene un ampersand lo que provoca que el comando se ejecute'
+#echo 'en segundo plano. Si no este comando podría pausar los builds.'
+#echo 'El "npm start" va seguido de otro comando que devuelvo el PID of proceso anterior'
+#echo 'y lo escribe en el .pidfile.'
+#
+#set -x
+#npm start &
+#sleep 1
+#
+#echo $! > .pidfile
+#set +x
+
 set -x
 cd app/react
-npm run build
-set +x
-
-echo 'El comando "npm" ejecuta la app Node.js/React.'
-echo 'El "npm start" tiene un ampersand lo que provoca que el comando se ejecute'
-echo 'en segundo plano. Si no este comando podría pausar los builds.'
-echo 'El "npm start" va seguido de otro comando que devuelvo el PID of proceso anterior'
-echo 'y lo escribe en el .pidfile.'
-
-set -x
 npm start &
 sleep 1
 
