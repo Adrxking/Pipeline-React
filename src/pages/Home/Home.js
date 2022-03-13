7e498ed1e56e89b955a137ca5b90a5eebf3b2a90
iona27 <pillancariona@gmail.com>
2022-03-13 19:46:20 +0100

    Titulo cambiado

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,30 +2,30 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 export default function Home() {
-  const history = useHistory();
-  
-  const postPage = () => {
-      history.push("/posts")
-  }
+    const history = useHistory();
 
-  const signInPage = () => {
-      history.push("/signin")
-  }
+    const postPage = () => {
+        history.push("/posts")
+    }
 
-  const signUpPage = () => {
-      history.push("/signup")
-  }
+    const signInPage = () => {
+        history.push("/signin")
+    }
 
-  return <div>
-      <h1>Queremos terminar el ciclo !</h1>
-      <button className="btn btn-success"
-          onClick={postPage}>Visitar todos los POSTS!!!!!!!
-      </button>
-      <button className="btn btn-light"
-          onClick={signInPage}>Iniciar Sesión
-      </button>
-      <button className="btn btn-info"
-          onClick={signUpPage}>Regístrate
-      </button>
-  </div>;
+    const signUpPage = () => {
+        history.push("/signup")
+    }
+
+    return <div>
+        <h1>**Aplicación usando REACT y GRAPHQL**</h1>
+        <button className="btn btn-success"
+            onClick={postPage}>Visitar todos los POSTS!!!!!!!
+        </button>
+        <button className="btn btn-light"
+            onClick={signInPage}>Iniciar Sesión
+        </button>
+        <button className="btn btn-info"
+            onClick={signUpPage}>Regístrate
+        </button>
+    </div>;
 }
